f56d5c994371ad5880413eab1d67d3b425204377
PedroSuarezCastaño <pepiscor@gmail.com>
2018-06-11 22:25:52 +0200

    google maps prueba

--- src/pages/mapa/mapa.ts
+++ src/pages/mapa/mapa.ts
@@ -1,5 +1,5 @@
-import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { Component, ViewChild, ElementRef } from '@angular/core';
+import { IonicPage, NavController, NavParams , Platform  } from 'ionic-angular';
 
 /**
  * Generated class for the MapaPage page.
@@ -8,18 +8,69 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
  * Ionic pages and navigation.
  */
 
+declare var google;
+let map: any;
+let infowindow: any;
+let options = {
+  enableHighAccuracy: true,
+  timeout: 5000,
+  maximumAge: 0
+};
 @IonicPage()
 @Component({
   selector: 'page-mapa',
   templateUrl: 'mapa.html',
 })
 export class MapaPage {
-
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  @ViewChild('map') mapElement: ElementRef;
+  constructor(public navCtrl: NavController, public navParams: NavParams, public platform: Platform) {
+    platform.ready().then(() => {
+      this.initMap();
+    });
   }
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad MapaPage');
+  }
+
+  initMap() {
+    navigator.geolocation.getCurrentPosition((location) => {
+      console.log(location);
+      map = new google.maps.Map(this.mapElement.nativeElement, {
+        center: {lat: location.coords.latitude, lng: location.coords.longitude},
+        zoom: 15
+      });
+  
+      infowindow = new google.maps.InfoWindow();
+      var service = new google.maps.places.PlacesService(map);
+      service.nearbySearch({
+        location: {lat: location.coords.latitude, lng: location.coords.longitude},
+        radius: 1000,
+        type: ['store']
+      }, (results,status) => {
+        if (status === google.maps.places.PlacesServiceStatus.OK) {
+          for (var i = 0; i < results.length; i++) {
+            this.createMarker(results[i]);
+          }
+        }
+      });
+    }, (error) => {
+      console.log(error);
+    }, options);
+    var myplace = {lat: -33.8665, lng: 151.1956};
+  }
+
+  createMarker(place) {
+    var placeLoc = place.geometry.location;
+    var marker = new google.maps.Marker({
+      map: map,
+      position: placeLoc
+    });
+  
+    google.maps.event.addListener(marker, 'click', function() {
+      infowindow.setContent(place.name);
+      infowindow.open(map, this);
+    });
   }
 
 }
